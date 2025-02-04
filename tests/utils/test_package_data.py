8392e547e6e38cadf3bd98be65d4fc9416f15e83
Joschka Roffe <joshuaroffe@gmail.com>
2025-02-04 17:39:47 +0000

    update test for package data installation; improve readability and add comments

--- tests/utils/test_package_data.py
+++ tests/utils/test_package_data.py
@@ -1,10 +1,14 @@
 import importlib.resources as resources
 import json
 
+### This test confirms that package data is correctly installed by pip 
+
 def test_package_data_correctly_installed():
     # Read and parse JSON file from package data
-    with resources.files("qec").joinpath("code_instances/saved_codes/test.json").open("r") as f:
+    with resources.files("qec").joinpath("code_instances/saved_codes/test.json").open(
+        "r"
+    ) as f:
         data = json.load(f)
 
-    # Print dictionary
-    assert data['hello'] == 'world'
+    # We can change this to an actual code file once we have decided on the save format.
+    assert data["hello"] == "world"
