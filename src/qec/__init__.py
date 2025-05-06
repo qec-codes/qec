1cd8df55eb644d35cd52db10ced4e9c33e9e086a
Joschka Roffe <joshuaroffe@gmail.com>
2025-05-06 15:53:12 +0100

    refactor: remove unused imports and __all__ definition from __init__.py

--- src/qec/__init__.py
+++ src/qec/__init__.py
@@ -1,10 +1,2 @@
-from qec import code_constructions, utils, circuit_compilation, code_instances, codetables_de
 
-__all__ = [
-    code_constructions,
-    utils,
-    circuit_compilation,
-    code_instances,
-    codetables_de
-]
 
